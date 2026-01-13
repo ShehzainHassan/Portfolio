a449cce55f631e9143a06c5e35d1c5f7139a8386
Shehzain Hassan <shehzainhassan@gmail.com>
2026-01-13 22:12:08 +0500

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { ThemeProvider } from "@/providers";
 
@@ -28,8 +29,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider >{children}</ThemeProvider>
+        <ThemeProvider>{children}</ThemeProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
